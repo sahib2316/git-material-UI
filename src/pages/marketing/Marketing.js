b1a2298fe340f047bfa0ffd8836610b49797bacb
Sahib <sahib.m1911@gmail.com>
2023-05-03 14:42:55 +0530

    Material-UI project first commit

---
+++ src/pages/marketing/Marketing.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { withRouter } from "react-router-dom";
+
+function Marketing() {
+  return (
+    <>
+      <div>
+        <h1>User List</h1>
+        <table>
+          <th>
+            <td>Firstname</td>
+            <td>Lastname</td>
+            <td>Username</td>
+            <td>Role</td>
+          </th>
+          <tr>
+            <td>Sahib</td>
+            <td>Malhotra</td>
+            <td>sahib2316</td>
+            <td>Developer</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default withRouter(Marketing);
